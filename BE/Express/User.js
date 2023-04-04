a381571208bb749695598c5bdf939be0d71c6a9a
haitrieu123580 <haitrieu123580@gmail.com>
2023-04-04 16:27:53 +0700

    CRUD user with express

---
+++ BE/Express/User.js
@@ -0,0 +1,15 @@
+const users = [
+	{
+		"id": 1,
+		"fullname": "Nguyen Huy Tuong",
+		"gender": true,
+		"age": 18
+	},
+	{
+		"id": 2,
+		"fullname": "Nguyen Thi Tuong",
+		"gender": false,
+		"age": 15
+	}
+]
+module.exports = users
